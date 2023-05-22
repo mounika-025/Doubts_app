634b7def7f2af298253720e8b9145deed92ff37e
mouni025 <im.mounika025@gmail.com>
2023-05-22 23:14:35 +0530

    update auth ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import AllRoutes from './AllRoutes';
-import {BrowserRouter} from 'react-router-dom'
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import AllRoutes from "./AllRoutes";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar/>
-      <AllRoutes/>
+        <Navbar />
+        <AllRoutes />
       </BrowserRouter>
-     
     </div>
   );
 }
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,11 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import icon from "../../assests/icon.png";
+import "./Auth.css";
+import AboutAuth from "./AboutAuth";
+const Auth = () => {
+  const [isSignedUp, setIsSignedUp] = useState(false);
 
-const Auth =()=>{
-    return(
-        <div>
-            <h1>Auth</h1>
-        </div>
-    )
-}
+  const handleAuth = () => {
+    setIsSignedUp(!isSignedUp);
+  };
+  return (
+    <section className="auth-page-section">
+      {isSignedUp && <AboutAuth />}
+      <div className="auth-container">
+        {!isSignedUp && (
+          <img src={icon} alt="stack-overflow" className="login-icon" />
+        )}
+        <form>
+          {isSignedUp && (
+            <label htmlFor="name">
+              <h4>Display Name</h4>
+              <input type="text" id="name" name="name" />
+            </label>
+          )}
+          <label htmlFor="email">
+            <h4>Email</h4>
+            <input type="email" name="email" id="email" />
+          </label>
+          <label htmlFor="password">
+            <div>
+              <h4>Password</h4>
+              {!isSignedUp && <h4>Forgot password?</h4>}
+            </div>
+            <input type="password" name="password" id="password" />
+            {isSignedUp && (
+              <p style={{ color: "#666767", fontSize: "13px" }}>
+                Passwords must have at least 8 characters, <br />
+                contain at least two of the following: uppercase letters,
+                lowercase letters,
+                <br /> numbers, and symbols.
+              </p>
+            )}
+          </label>
+          {/* {isSignedUp && (<label htmlFor="check"><input type="checkbox" id="check"/></label>)} */}
+          <button type="submit" className="auth-btn">
+            {isSignedUp ? "Sign Up" : "Login"}
+          </button>
+          {isSignedUp && (
+            <p style={{ color: "#666767", fontSize: "13px" }}>
+              By clicking "Sign Up", you agree to our{" "}
+              <span style={{ color: "#007ac6" }}>
+                terms of <br /> service
+              </span>
+              ,<span style={{ color: "#007ac6" }}>privacy policy</span> and
+              <span style={{ color: "#007ac6" }}>cookie policy</span>
+            </p>
+          )}
+        </form>
+        <p>
+          {isSignedUp ? "Already have an account?" : "Create a new account"}
+          <button
+            type="button"
+            className="auth-handle-btn"
+            onClick={handleAuth}
+          >
+            {isSignedUp ? "Login" : "Sign Up"}
+          </button>
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default Auth;
